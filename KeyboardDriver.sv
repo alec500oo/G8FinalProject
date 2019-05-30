32b843086e3c7806b3f93b4dc9ec9b3dd455a5d0
Alec Matthews <alec500oo@gmail.com>
2019-05-30 15:26:49 -0700

    Got a simple version of the serial decoder added.
    Added simple shift register implementation of serial. There is no error
    checking or command identification implemented yet.

--- KeyboardDriver.sv
+++ KeyboardDriver.sv
@@ -4,8 +4,32 @@ module KeyboardDriver (input logic clk, // Clock driven by device
                        output logic[7:0] code, // Output code
                        output logic ext, // Extended key code (Arrow, Ctrl...)
                        output logic back); // "Back" received, key released.
-    logic[9:0] storageRegister;
 
+    logic[9:0] storageRegister; // Ext in bit 9, back in bit 8, data [7:0]
+
+    // Data shift register.
+    logic[10:0] dataShift; // 11 bit data shift register 
+    always_ff @(negedge clk, negedge reset_n) begin
+        if (!reset_n) dataShift <= 11'b0;
+        else dataShift <= {dataShift[9:0], data};
+    end
+
+    // Data in counter.
+    logic[3:0] dataCount;
+    logic dataLatch;
+    always_ff @(negedge clk, negedge reset_n) begin
+        if (!reset_n) dataCount <= 4'b0;
+        else dataCount <= (dataCount + 'b1);
+    end
+    assign dataLatch = (dataCount >= 11) ? 'b1 : 'b0; // Latch data when count is over 11.
+
+    // Data latch
+    /* Assign the data from the incomming message and flip it around to
+       MSB first. */
+    always_ff @(posedge dataLatch, negedge reset_n) begin
+        if (!reset_n) storageRegister <= 10'b0;
+        else storageRegister[7:0] <= dataShift[8:1];
+    end
 
     // Split the storage regitser into the output lines.
     assign ext = storageRegister[9];
