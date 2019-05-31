f084159d771091421e97435261224249aa515369
Alec Matthews <alec500oo@gmail.com>
2019-05-30 23:31:22 -0700

    Began adding state machine logic to the keyboard module.
    To decode keys it is required that a state machine monitors input
    packets to mark the special control packets that can come from the
    keyboard. To do this, a state machine is used to check incoming packets
    and set special output bits that are only latched when a new key press
    has been completely received from the keyboard. Once this output is
    available the key press can be deocded to an ASCII character.

--- KeyboardDriver.sv
+++ KeyboardDriver.sv
@@ -1,11 +1,40 @@
-module KeyboardDriver (input logic clk, // Clock driven by device
+module KeyboardDriver (input logic data_clk, // Clock driven by device
                        input logic data, // Data in (serially driven)
+                       input logic clk, // Internal FPGA clock for state machine.
                        input logic reset_n, // Reset active LOW
                        output logic[7:0] code, // Output code
                        output logic ext, // Extended key code (Arrow, Ctrl...)
                        output logic back); // "Back" received, key released.
 
     logic[9:0] storageRegister; // Ext in bit 9, back in bit 8, data [7:0]
+
+    // State machine logic
+    typedef enum logic[2:0] { rec, checkData, setBa, setExt, latchOut, clear } t_state;
+    t_state[2:0] state, nextState; // State registers.
+
+    // State Register
+    always_ff @(posedge clk, negedge reset_n) begin
+        if (!reset_n) state <= clear;
+        else state <= nextState;
+    end
+
+    // Next logic
+    logic data_in;
+    logic[7:0] data;
+    always_comb begin
+        case({state, data, data_in})
+            {rec, 8'bx, 'b0}: nextState <= rec;
+            {rec, 8'bx, 'b1}: nextState <= checkData;
+            {checkData, 'hF0, 'bx}: nextState <= setBa; // TODO: Might need to set constant to 'h0F because data comes
+            {setBa, 'hF0, 'bx}: nextState <= rec;       // in LSB first.
+            {checkData, 'hE0, 'bx}: nextState <= setExt; // TODO: Might need to set constant to 'h0E because data comes
+            {setExt, 'hE0, 'bx}: nextState <= rec;       // in LSB first.
+            {checkData, 8'bx, 'bx}: nextState <= latchOut;
+            {latchOut, 8'bx, 'bx}: nextState <= clear;
+            {clear, 8'bx, 'bx}: nextState <= rec;
+            default: nextState <= clear;
+        endcase
+    end
 
     // Data shift register.
     logic[10:0] dataShift; // 11 bit data shift register 
